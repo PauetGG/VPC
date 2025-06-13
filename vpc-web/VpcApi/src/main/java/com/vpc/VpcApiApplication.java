30db1f0cd986cda071391d6e072a64f5a4d997d4
PauetGG <paugallardogiralt@gmail.com>
2025-06-13 13:51:18 +0200

    Commit de momento

--- vpc-web/VpcApi/src/main/java/com/vpc/VpcApiApplication.java
+++ vpc-web/VpcApi/src/main/java/com/vpc/VpcApiApplication.java
@@ -12,18 +12,5 @@ public class VpcApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(VpcApiApplication.class, args);
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // en vez de solo /greeting-javaconfig
-                        .allowedOrigins("http://localhost:3000") // aquí pones tu Next.js
-                        .allowedMethods("*") // permite GET, POST, PUT, DELETE, OPTIONS, etc.
-                        .allowedHeaders("*");
-            }
-        };
-    }
 }
 
